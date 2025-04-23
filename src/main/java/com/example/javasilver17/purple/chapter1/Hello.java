35091b6edb2b7a06eecc31768564d606340f426e
arta-crypt <8yt84m.wa3.kg34a@gmail.com>
2025-04-23 23:09:44 +0900

    Initial commit

---
+++ src/main/java/com/example/javasilver17/purple/chapter1/Hello.java
@@ -0,0 +1,8 @@
+package com.example.javasilver17.purple.chapter1;
+
+public class Hello {
+    public static void main(String... args) {
+        // 処理
+        System.out.println("Hello world!");
+    }
+}
