6a89c4999b11fc5e170c9aa0b6ccef26a6090945
arta-crypt <8yt84m.wa3.kg34a@gmail.com>
2025-05-16 20:42:37 +0900

    feat: String.intern()の動作確認サンプルを追加
    - String.intern()の動作を実証するサンプルコードを追加
    - 文字列リテラルとnew String()の違いを示す比較コードを含む
    - intern()メソッドの動作を確認するためのテストケースを実装

---
+++ src/main/java/com/example/javasilver17/knowledge/interning/InterningSample.java
@@ -0,0 +1,19 @@
+package com.example.javasilver17.knowledge.interning;
+
+public class InterningSample {
+    public static void main(String... args) {
+        String s_literal1 = "Hello";
+        String s_literal2 = "Hello";
+        String s_new1 = new String("Hello");
+        String s_new2 = new String("Hello");
+        String s_intern1 = s_literal1.intern();
+        String s_intern2 = s_new1.intern();
+
+        System.out.println(s_literal1 == s_literal2); // true
+        System.out.println(s_new1 == s_new2); // false
+        System.out.println(s_intern1 == s_intern2); // false
+        System.out.println(s_literal1 == s_new1); // false
+        System.out.println(s_literal1 == s_intern1); // true
+        System.out.println(s_new1 == s_intern1); // true
+    }
+}
